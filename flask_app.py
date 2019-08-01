7ad11abd5bb1434b4f38cd5b04850278d8a2cf22
Luc Larocque <luc.larocque@uwaterloo.ca>
2019-08-01 16:39:43 +0000

    Added database for comments

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,23 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 from montecarlo import simulate
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="lucwashere",
+    password="lucdb27921",
+    hostname="lucwashere.mysql.pythonanywhere-services.com",
+    databasename="lucwashere$comments>",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 comments = []
 @app.route('/', methods=["GET", "POST"])
