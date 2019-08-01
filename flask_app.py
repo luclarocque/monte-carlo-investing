94ab5c598f1d65e78992d07d9b4c712ff9f99918
Luc Larocque <luc.larocque@uwaterloo.ca>
2019-08-01 16:20:56 +0000

    Replaced dummy comments with comment list stored in-memory

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,19 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from montecarlo import simulate
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
+comments = []
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"]) # Else: this is a POST
+    return redirect(url_for('index'))
+
 
