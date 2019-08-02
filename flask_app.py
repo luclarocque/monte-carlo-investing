874bcb48be70b9dcb1f624ac01b9807ca6fb66a1
Luc Larocque <luc.larocque@uwaterloo.ca>
2019-08-02 19:09:19 +0000

    Image displays correctly. Site prototype working.

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,9 @@
 from flask import Flask, redirect, render_template, request, url_for
 # from flask_sqlalchemy import SQLAlchemy  # use this if database required
 from montecarlo import simulate
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+plot_filename = os.path.join(THIS_FOLDER, 'assets/images/plot.png')
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -12,6 +15,7 @@ app.config["DEBUG"] = True
 @app.route('/', methods=["GET", "POST"])
 def index():
     result = None
+    plot = ""
     errors = ""
     if request.method == "POST":
         pv = None
@@ -19,41 +23,48 @@ def index():
         t = None
         r = None
         sd = 11.4
+
+        # validate input
         try:
             pv = float(request.form["pv"])
             if pv == None:
-                errors += "<p>You must fill enter a number in all fields.</p>\n"
+                errors += "You must enter a number in all fields.\n"
         except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["pv"])
+            errors += "{} is not a number.\n".format(request.form["pv"])
         try:
             pmt = float(request.form["pmt"])
             if pmt == None:
-                errors += "<p>You must fill enter a number in all fields.</p>\n"
+                errors += "You must enter a number in all fields.\n"
         except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["pmt"])
+            errors += "{} is not a number.\n".format(request.form["pmt"])
         try:
             t = int(request.form["t"])
             if t <= 0:
-                errors += "<p>{!r} must be greater than 0.</p>\n".format(request.form["t"])
+                errors += "Number of years (t) must be greater than 0.\n".format(request.form["t"])
         except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["t"])
+            errors += "{} is not a number.\n".format(request.form["t"])
         try:
             r = float(request.form["r"])
             if r < 0:
-                errors += "<p>{!r} must be greater than or equal to 0.</p>\n".format(request.form["r"])
+                errors += "Annual interest (I/Y) must be greater than or equal to 0.\n".format(request.form["r"])
         except:
-            errors += "<p>{!r} is not a number.</p>\n".format(request.form["t"])
+            errors += "{} is not a number.\n".format(request.form["t"])
 
         # if errors occurred
         if errors != "":
-            return redirect(url_for('index'))
+            return render_template("monte_carlo.html", errors=errors)
+            # return redirect(url_for('index'))
 
         # variables defined. Apply simulate().
         result = simulate(pv, pmt, t, r, sd)
-        return render_template("monte_carlo.html", errors=errors, result=result)
+        plot = plot_filename
+        return render_template("monte_carlo.html", errors=errors, result=result, plot=plot)
 
     if request.method == "GET":
-        return render_template("monte_carlo.html", errors=errors, result=result)
+        if result == None:
+            return render_template("monte_carlo.html", errors=errors)
+        else:
+            return render_template("monte_carlo.html", errors=errors, result=result, plot=plot)
 
 
 
--- montecarlo.py
+++ montecarlo.py
@@ -5,6 +5,9 @@ import matplotlib as mpl
 import matplotlib.gridspec as gridspec
 from scipy.stats import percentileofscore
 from finlib import create_rand_array, recession_adjustment, ror_with_pmts
+import os
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+out_filename = os.path.join(THIS_FOLDER, 'assets/images/plot.png')
 
 
 def simulate(PV, PMT, t, r, sd, N=1000, peryear=12):
@@ -62,8 +65,9 @@ def simulate(PV, PMT, t, r, sd, N=1000, peryear=12):
         print("{}% chance of ending with more than ${:,.0f}".format(100-p, int(percentiles[p])))
     print("---"*10)
     break_even = PV+PMT*n
-    if PMT > 0:
-        print("Probability of breaking even with more than ${:,.0f}: {:,.1f}%".format(break_even, 100-percentileofscore(res_arr, break_even)))
+    if PMT < 0:
+        break_even = PV
+    print("Probability of breaking even with more than ${:,.0f}: {:,.1f}%".format(break_even, 100-percentileofscore(res_arr, break_even)))
     print("Probability of getting more than ${:,.0f}: {:,.1f}%".format(1e6, 100-percentileofscore(res_arr, 1e6)))
 
 
@@ -74,9 +78,9 @@ def simulate(PV, PMT, t, r, sd, N=1000, peryear=12):
 
     # plot 1 (histogram) -------------------------------------------------
     ax1.title.set_text("Starting with ${:,.0f} over {} years with payments of {:,.0f}".format(PV, t, PMT))
-    ax1.text(percentiles[50]-0.05*percentiles[50], 3, "50%:\n{:,}".format(int(percentiles[50])))
+    ax1.text(percentiles[50], 3, "50%:\n{:,}".format(int(percentiles[50])), color='w')
     ax1.axvline(x=percentiles[50], color='k')  # plot median line
-    ax1.text(percentiles[5]-0.05*percentiles[5], 3, "5%:\n{:,}".format(int(percentiles[5])))
+    ax1.text(percentiles[5], 3, "5%:\n{:,}".format(int(percentiles[5])), color='w')
     ax1.axvline(x=percentiles[5], color='k')  # plot 5th percentile line
 
     # trimbins = trim_bins(bins)  # remove extraneous bins from both ends
@@ -141,8 +145,8 @@ def simulate(PV, PMT, t, r, sd, N=1000, peryear=12):
 
     # final step---------------------------------------------------------
     plt.plot()
-    return percentiles[50]
-
+    plt.savefig(out_filename)
+    return "${:,.2f}".format(percentiles[50])
 
 
 if __name__ == "__main__":
@@ -171,4 +175,3 @@ if __name__ == "__main__":
     simulate(PV, PMT, years, ROR, sd)
 
     plt.show()
-    # pass
